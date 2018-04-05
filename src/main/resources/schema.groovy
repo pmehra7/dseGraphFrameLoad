6e2c96e828d959053d6af9b32e6c1fd63c1d8531
Paras Mehra <parasmehra37@gmail.com>
2018-04-05 16:01:37 -0400

    Update schema.groovy

--- src/main/resources/schema.groovy
+++ src/main/resources/schema.groovy
@@ -1,91 +1,87 @@
-system.graph('Test_Graph').create()
+system.graph('Test_Graph').ifNotExists().create()
 :remote config alias g Test_Graph.g
 
 // Property Keys
 
-schema.propertyKey("offer").Text().create()
-schema.propertyKey("category").Text().create()
-schema.propertyKey("quantity").Int().create()
-schema.propertyKey("company").Text().create()
-schema.propertyKey("offervalue").Double().create()
-schema.propertyKey("brand").Text().create()
-schema.propertyKey("chain").Text().create()
-schema.propertyKey("dept").Text().create()
-schema.propertyKey("productsize").Double().create()
-schema.propertyKey("productmeasure").Text().create()
-schema.propertyKey("purchasequantity").Int().create()
-schema.propertyKey("purchaseamount").Double().create()
-schema.propertyKey("customer_id").Text().create()
-schema.propertyKey("date").Timestamp().create()
-schema.propertyKey("market").Int().create()
-schema.propertyKey("repeater").Text().create()
-schema.propertyKey("repeattrips").Int().create()
+schema.propertyKey("offer").Text().ifNotExists().create()
+schema.propertyKey("category").Text().ifNotExists().create()
+schema.propertyKey("quantity").Int().ifNotExists().create()
+schema.propertyKey("company").Text().ifNotExists().create()
+schema.propertyKey("offervalue").Double().ifNotExists().create()
+schema.propertyKey("brand").Text().ifNotExists().create()
+schema.propertyKey("chain").Text().ifNotExists().create()
+schema.propertyKey("dept").Text().ifNotExists().create()
+schema.propertyKey("productsize").Double().ifNotExists().create()
+schema.propertyKey("productmeasure").Text().ifNotExists().create()
+schema.propertyKey("purchasequantity").Int().ifNotExists().create()
+schema.propertyKey("purchaseamount").Double().ifNotExists().create()
+schema.propertyKey("customer_id").Text().ifNotExists().create()
+schema.propertyKey("date").Timestamp().ifNotExists().create()
+schema.propertyKey("market").Int().ifNotExists().create()
+schema.propertyKey("repeater").Text().ifNotExists().create()
+schema.propertyKey("repeattrips").Int().ifNotExists().create()
 
 // VERTICES
 
-schema
-        .vertexLabel("offer")
-        .partitionKey("offer")
-//.clusteringKey("category")
-        .properties(
+schema.vertexLabel("offer").
+      partitionKey("offer").
+      //.clusteringKey("category")
+      properties(
         "category",
         "quantity",
         "company",
         "offervalue",
         "brand"
-).create()
+).ifNotExists().create()
 
-schema
-        .vertexLabel("product")
-        .partitionKey("chain", "company", "brand")
-        .clusteringKey("dept", "category")
-        .properties(
+schema.vertexLabel("product").
+       partitionKey("chain", "company", "brand").
+       clusteringKey("dept", "category").
+       properties(
         "productsize",
         "productmeasure",
         "purchasequantity",
         "purchaseamount",
         "customer_id",
         "date"
-).create()
+).ifNotExists().create()
 
-schema
-        .vertexLabel("store")
-        .partitionKey("chain")
-        .properties(
+schema.vertexLabel("store").
+       partitionKey("chain").
+       properties(
         "market"
 ).create()
 
-schema
-        .vertexLabel("customer")
-        .partitionKey("customer_id")
-        .create()
+schema.vertexLabel("customer").
+       partitionKey("customer_id").
+       ifNotExists().create()
 
 // EDGES
 
-schema.edgeLabel("purchases")
-        .multiple()
-        .properties(
+schema.edgeLabel("purchases").
+       multiple().
+       properties(
         "date",
         "purchasequantity",
         "purchaseamount"
-)
-        .connection("customer", "product")
-        .create()
+        ).
+       connection("customer", "product").
+       ifNotExists().create()
 
-schema.edgeLabel("offer_used")
-        .multiple()
-        .properties(
+schema.edgeLabel("offer_used").
+       multiple().
+       properties(
         "date",
         "repeater",
         "repeattrips"
-)
-        .connection("customer", "offer")
-        .create()
+        ).
+       connection("customer", "offer").
+       ifNotExists().create()
 
-schema.edgeLabel("visits")
-        .multiple()
-        .properties(
+schema.edgeLabel("visits").
+       multiple().
+       properties(
         "date"
-)
-        .connection("customer", "store")
-        .create()
+       ).
+       connection("customer", "store").
+       ifNotExists().create()
